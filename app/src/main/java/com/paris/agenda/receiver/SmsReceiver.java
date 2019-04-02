00cf23f895cde506a1dd846536f30c2dc3479b5b
Renan Paris <paris.oliveira@gmail.com>
2019-04-02 17:18:34 -0300

    Adição da classe SmsReceiver e permissão para SMS receiver

---
+++ app/src/main/java/com/paris/agenda/receiver/SmsReceiver.java
@@ -0,0 +1,13 @@
+package com.paris.agenda.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class SmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Chegou uma Nova Menssagem", Toast.LENGTH_SHORT).show();
+    }
+}
