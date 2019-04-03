a60404cf90491172d409d48ea4828b8c274df326
Renan Paris <paris.oliveira@gmail.com>
2019-04-03 17:16:14 -0300

    Envio das notas ao servidor e recebimento das médias

---
+++ app/src/main/java/com/paris/agenda/SendStudentTask.java
@@ -0,0 +1,50 @@
+package com.paris.agenda;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.paris.agenda.com.paris.agenda.db.StudentDao;
+import com.paris.agenda.json.StudentConvert;
+import com.paris.agenda.modelo.Student;
+
+import java.util.List;
+
+public class SendStudentTask extends AsyncTask<Void, Void, String> {
+
+    private Context context;
+    private ProgressDialog dialog;
+
+    public SendStudentTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+
+        StudentDao dao = new StudentDao(context);
+        List<Student> students = dao.searchStudents();
+        dao.close();
+        StudentConvert convert = new StudentConvert();
+        String json = convert.convertToJSON(students);
+
+        WebClient client = new WebClient();
+        String responseServer = client.post(json);
+
+        return responseServer;
+    }
+
+    @Override
+    protected void onPreExecute() {
+       dialog = ProgressDialog.show(context,
+                "Aguarde", "Enviando Aluno", true, true);
+    }
+
+    @Override
+    protected void onPostExecute(String responseServer) {
+        dialog.dismiss();
+
+        Toast.makeText(context, responseServer, Toast.LENGTH_LONG).show();
+    }
+}
