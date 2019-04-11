e81569cfbe8deb08bca51324cfdf0c59e2b6d492
Renan Paris <paris.oliveira@gmail.com>
2019-04-11 19:11:58 -0300

    Adição do fragment para as telas de prova e detalhes de prova.

---
+++ app/src/main/java/com/paris/agenda/TestFragment.java
@@ -0,0 +1,62 @@
+package com.paris.agenda;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.paris.agenda.modelo.Test;
+import com.paris.agenda.ui.DetailsTestActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.activity_fragment_test, container, false);
+        List<String> themesPortguese = new ArrayList<>();
+        List<String> themesMath = new ArrayList<>();
+        themesPortguese.add("Sujeito");
+        themesPortguese.add("Objeto");
+        themesPortguese.add("Predicado");
+        themesMath.add("Algebra");
+        themesMath.add("Matriz");
+        themesMath.add("Vetores");
+
+        List<Test> tests = new ArrayList<>();
+
+        Test portuguese = new Test("Português", "25/04/2019",themesPortguese );
+        Test math = new Test("Matemática", "26/04/2019", themesMath);
+
+        tests.add(portuguese);
+        tests.add(math);
+
+        ArrayAdapter<Test> adapter = new ArrayAdapter<>(view.getContext(), android.R.layout.simple_list_item_1, tests);
+        ListView list = view.findViewById(R.id.activity_list_tests);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Test test = (Test) parent.getItemAtPosition(position);
+                Intent goToDetails = new Intent(view.getContext(), DetailsTestActivity.class);
+                goToDetails.putExtra("test", test);
+                startActivity(goToDetails);
+            }
+        });
+
+
+        return view;
+    }
+}
--- app/src/main/java/com/paris/agenda/ui/TestActivity.java
+++ app/src/main/java/com/paris/agenda/ui/TestActivity.java
@@ -1,6 +1,8 @@
 package com.paris.agenda.ui;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +14,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.paris.agenda.R;
+import com.paris.agenda.TestFragment;
 import com.paris.agenda.modelo.Test;
 
 import java.lang.reflect.Array;
@@ -25,36 +28,10 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        List<String> themesPortguese = new ArrayList<>();
-        List<String> themesMath = new ArrayList<>();
-        themesPortguese.add("Sujeito");
-        themesPortguese.add("Objeto");
-        themesPortguese.add("Predicado");
-        themesMath.add("Algebra");
-        themesMath.add("Matriz");
-        themesMath.add("Vetores");
-
-        List<Test> tests = new ArrayList<>();
-
-        Test portuguese = new Test("Português", "25/04/2019",themesPortguese );
-        Test math = new Test("Matemática", "26/04/2019", themesMath);
-
-        tests.add(portuguese);
-        tests.add(math);
-
-        ArrayAdapter<Test> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tests);
-         ListView list = findViewById(R.id.activity_list_tests);
-         list.setAdapter(adapter);
-
-         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-             @Override
-             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                 Test test = (Test) parent.getItemAtPosition(position);
-                 Intent goToDetails = new Intent(TestActivity.this, DetailsTestActivity.class);
-                 goToDetails.putExtra("test", test);
-                 startActivity(goToDetails);
-             }
-         });
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fx = fragmentManager.beginTransaction();
+        fx.replace(R.id.activity_test_frame, new TestFragment());
+        fx.commit();
 
 
     }
